477bbb52b6e388c60e5de4fbcfa9ff8daaf3859e
trainb0y <trainb0ydev@gmail.com>
2023-02-04 16:45:10 -0700

    Exploding boxes

---
+++ Assets/Scripts/BoxExplosion.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BoxExplosion : MonoBehaviour
+{
+    [SerializeField] private float threshold;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.relativeVelocity.magnitude > threshold)
+        {
+            var p = GetComponent<ParticleSystem>();
+            p.Stop();
+            p.Play();
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
+            Destroy(gameObject, 4);
+            GlobalData.Instance.cashAmount += 10;
+        }
+    }
+}
